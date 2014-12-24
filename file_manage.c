b1141a91ed122a3663c390a16b7facfaeaf46064
Lang Yu <y_u_lang@163.com>
2014-12-24 22:16:04 +0800

    modification

--- file_manage.c
+++ file_manage.c
@@ -10,7 +10,12 @@ void abspath_gen(char* path, char* filename, char* abs_path)
 	strcat(abs_path, filename);
 }
 
-void write_conf(char* path, char* lig_name, char* rcp_name, int thread_num) 
+void write_conf(char* path, char* lig_name, char* rcp_name, int thread_num, int index, int is_mic) 
 {
 	FILE* f_hdl;
+	int len = strlen(path) + (index/10 + 1) + 1;
+	char* conf_path = (char*) malloc(len);
+
+
+
 }
