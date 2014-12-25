80796e9eaae09dd4b2a658ac555466f4860b7e1d
Lang Yu <y_u_lang@163.com>
2014-12-25 21:24:45 +0800

    Revert "file manage"
    This reverts commit cd8eda3b4036f54335228afe8be95c3b841dfde9.

--- file_manage.c
+++ file_manage.c
@@ -1,6 +1,5 @@
 #include <file_manage.h>
 #include <sys/stat.h>
-#include <stdlib.h>
 #include <string.h>
 
 void abspath_gen(char* path, char* filename, char* abs_path);
@@ -14,11 +13,9 @@ void abspath_gen(char* path, char* filename, char* abs_path)
 void write_conf(char* path, char* lig_name, char* rcp_name, int thread_num, int index, int is_mic) 
 {
 	FILE* f_hdl;
-	char* fwstream;
-	int len = strlen(path) + strlen("conf_") + (index/10) + strlen(".txt") + 1;
-	char *conf_path = (char*) malloc(len), *tmp = (char*) malloc(MAX_INDEX + 1);
-	itoa(index, tmp, 10);
-	strcpy(conf_path, "conf_");
-	strcat(conf_path, tmp);
+	int len = strlen(path) + (index/10 + 1) + 1;
+	char* conf_path = (char*) malloc(len);
+
+
 
 }
--- file_manage.h
+++ file_manage.h
@@ -1,7 +1,6 @@
 #ifndef FILE_MANAGE_H
 #define FILE_MANAGE_H
 
-#define MAX_INDEX 5
 void write_conf(char* path, char* lig_name, char* rcp_name, int thread_num);
 
 #endif
